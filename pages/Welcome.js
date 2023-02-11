ba1bb56a60f46aeafcec86b554211dac7dc2b932
heidenreichgeorg <georg.heidenreich@bexar.de>
2023-02-11 23:53:15 +0100

    begin UPLOAD

--- pages/Welcome.js
+++ pages/Welcome.js
@@ -1,85 +1,78 @@
-import { useEffect, useState } from 'react';
 
-import { useSession, REACT_APP_API_HOST } from '../modules/sessionmanager';
-
-import Screen from '../pages/Screen'
-import FooterRow from '../components/FooterRow'
-import { makeStatusData }  from '../modules/App';
-import { cents2EU }  from '../modules/money';
-
-import { D_Page } from '../modules/terms.js'
+import { PORT } from '../modules/terms.js'
+import { HTTP_OK, HTTP_WRONG } from '../modules/sessionModule.js'
+import { timeSymbol } from '../modules/writeModule.js'
+import { REACT_APP_API_HOST } from '../modules/sessionmanager.tsx';
 
 export default function Welcome() {
 
     function dragOverHandler(ev) {
-        console.log('File(s) in drop zone:'); //+JSON.stringify(ev.dataTransfer.items[0]));
         // Prevent default behavior (Prevent file from being opened)
         ev.preventDefault();
+        console.log('File(s) in drop zone'); //+JSON.stringify(ev.dataTransfer.items[0]));
     }
 
 
     function dropHandler(ev) {
-        console.log('File(s) dropped');
-        
         // Prevent default behavior (Prevent file from being opened)
         ev.preventDefault();
+        console.log('File(s) dropped');        
 
-        if (ev.dataTransfer.items) {
-        // Use DataTransferItemList interface to access the file(s)
-            
-            // If dropped items aren't files, reject them
-            if (ev.dataTransfer.items[0].kind === 'file') {
-                let file = ev.dataTransfer.files[0];
+            if (ev.dataTransfer.items) {
+            // Use DataTransferItemList interface to access the file(s)
+                
+                // If dropped items aren't files, reject them
+                if (ev.dataTransfer.items[0].kind === 'file') {
+                    let file = ev.dataTransfer.files[0];
 
-                console.log('File '+file.name+'  dropped' );
-/*
+                    console.log('File '+file.name+'  dropped' );
 
-                var fr = new FileReader();
-                fr.onload = function () {
+                    var fr = new FileReader();
+                    fr.onload = function () {
 
-                    let fileBuffer = this.result;            
-                        
-                    let jData={};
-                    try {
-                        jData = JSON.parse(fileBuffer);
-                        jData.clientFunction="Booking"; // GH20220926
+                        let fileBuffer = this.result;            
+                            
+                        let jData={};
+                        try {
+                            jData = JSON.parse(fileBuffer);
+                            jData.clientFunction="Booking"; // GH20220926
 
-                    } catch(err) { 
-                        //console.dir("WELCOMEDROP CANNOT PARSE "+fileBuffer); 
-                        let sheetLines = fileBuffer.split(CSV_RS);
-                        if(sheetLines.length>6) {
-                            console.error("0820 UPLOAD CSV WITH "+sheetLines.length+" lines");                  
-                            console.dir("\n0822 UPLOAD CSV N-line "+JSON.stringify(sheetLines[0])); 
-                            console.dir("\n0824 UPLOAD CSV K-line "+JSON.stringify(sheetLines[4]));                   
+                        } catch(err) { 
+                            //console.dir("WELCOMEDROP CANNOT PARSE "+fileBuffer); 
+                            let sheetLines = fileBuffer.split(CSV_RS);
+                            if(sheetLines.length>6) {
+                                console.error("0820 UPLOAD CSV WITH "+sheetLines.length+" lines");                  
+                                console.dir("\n0822 UPLOAD CSV N-line "+JSON.stringify(sheetLines[0])); 
+                                console.dir("\n0824 UPLOAD CSV K-line "+JSON.stringify(sheetLines[4]));                   
 
 
-                            // indices depend on Sheets.makeXLTabs
-                            let arrKLine = sheetLines[3].split(CSV_FS);
-                            let year  = arrKLine[2];
-                            let client= arrKLine[4];
-                            console.dir("\n0826 UPLOAD CSV client:"+client+"  year:"+year);                   
+                                // indices depend on Sheets.makeXLTabs
+                                let arrKLine = sheetLines[3].split(CSV_FS);
+                                let year  = arrKLine[2];
+                                let client= arrKLine[4];
+                                console.dir("\n0826 UPLOAD CSV client:"+client+"  year:"+year);                   
 
-                            let aoaSheet = sheetLines.map((strRow)=>(strRow.split(CSV_FS)));
+                                let aoaSheet = sheetLines.map((strRow)=>(strRow.split(CSV_FS)));
 
-                            jData.year  = year;
-                            jData.client= client;
-                            jData.sheetCells = JSON.parse(JSON.stringify(aoaSheet));
-                            jData.sheetName = ""+client+year;
-                            jData.clientFunction= "BeginYear";
+                                jData.year  = year;
+                                jData.client= client;
+                                jData.sheetCells = JSON.parse(JSON.stringify(aoaSheet));
+                                jData.sheetName = ""+client+year;
+                                jData.clientFunction= "BeginYear";
 
+                            }
                         }
+                        callServer('POST',"UPLOAD","application/json",JSON.stringify(jData),showResponse);
+
+
                     }
-                    callServer('POST',"UPLOAD","application/json",JSON.stringify(jData),showResponse);
-
-
+                    fr.readAsText(file);
                 }
-                fr.readAsText(item);
+    
             }
-  */
+            console.log('MANAGED');
         }
-        console.log('MANAGED');
-    }
-    }
+    
 
     function showResponse(response) {
         let display = document.getElementById('mainPage');
@@ -88,34 +81,35 @@ export default function Welcome() {
     }
 
     function callServer(cmd,strTarget,applicationType,strParams,callBack) {
-    // cmd = GET or POST
-    let strServerDNS = 'http://'+self.location.hostname+':'+PORT+'/';     
-    if(strTarget) {
-        var request = new XMLHttpRequest();       
-        console.log('\n'+cmd+' '+strServerDNS+strTarget+"?"+strParams+"\n");			
-        request.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status>=HTTP_OK) {
-                if(callBack) {                   
-                    if(this.status==HTTP_WRONG) callBack( ""+this.status+" ERROR " );
-                    else {
-                        callBack( request.responseText );
-                        console.log("callServer callback(responseText)");
+        // cmd = GET or POST
+        //let strServerDNS = 'http://'+self.location.hostname+':'+PORT+'/';     
+        let strServerDNS = REACT_APP_API_HOST+'/';
+        if(strTarget) {
+            var request = new XMLHttpRequest();       
+            console.log('\n'+cmd+' '+strServerDNS+strTarget+"?"+strParams+"\n");			
+            request.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status>=HTTP_OK) {
+                    if(callBack) {                   
+                        if(this.status==HTTP_WRONG) callBack( ""+this.status+" ERROR " );
+                        else {
+                            callBack( request.responseText );
+                            console.log("callServer callback(responseText)");
+                        }
                     }
                 }
-            }
-        };
-        request.open(cmd,strServerDNS+strTarget, true);						
-        request.addEventListener('error', handleEvent);
-        request.setRequestHeader('Cache-control', 'no-cache, must-revalidate, post-check=0, pre-check=0');
-        request.setRequestHeader('Cache-control', 'max-age=0');
-        request.setRequestHeader( 'Content-Type', applicationType+';charset=UTF-8'); //("Content-type", "text/json");			
-        request.send(strParams);
-    } else alert('callServer: no target:  '+cmd+' '+strServerDNS+strTarget+"?"+strParams+"\n");
+            };
+            request.open(cmd,strServerDNS+strTarget, true);						
+            request.addEventListener('error', handleEvent);
+            request.setRequestHeader('Cache-control', 'no-cache, must-revalidate, post-check=0, pre-check=0');
+            request.setRequestHeader('Cache-control', 'max-age=0');
+            request.setRequestHeader( 'Content-Type', applicationType+';charset=UTF-8'); //("Content-type", "text/json");			
+            request.send(strParams);
+        } else alert('callServer: no target:  '+cmd+' '+strServerDNS+strTarget+"?"+strParams+"\n");
     }
 
     function handleEvent(event) { 
-    if(event) console.log(timeSymbol()+'--'+event.type+': '+event.message+' '+event.filename+'\n'); 
-    else console.log(timeSymbol()+'--EMPTY');
+        if(event) console.log(timeSymbol()+'--'+event.type+': '+event.message+' '+event.filename+'\n'); 
+        else console.log(timeSymbol()+'--EMPTY');
     }
 
     return (        
@@ -125,9 +119,10 @@ export default function Welcome() {
                 CSV/JSON in UTF-8
             </div>
             <div className="attrPair">
-                <div className="FIELD FLEX" onDragOver={dragOverHandler} onDrop={dropHandler}> </div>                
-                <div className="FIELD key" id="fileupload">Upload</div>            
+                <div className="FLEX key" onDragOver={dragOverHandler} onDrop={dropHandler}> </div>                
+                
             </div>
+            <div className="key" id="fileupload">Upload</div>            
         </div>
          
         
