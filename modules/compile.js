4991dd15ceb275340b0d0e5d60272127e99b1f73
heidenreichgeorg <georg.heidenreich@bexar.de>
2024-01-02 17:52:46 +0100

    integrate Gain Loss page

--- modules/compile.js
+++ modules/compile.js
@@ -126,6 +126,7 @@ let de_DE = {
     InterestCost:"Zinskosten",
     IncomeTax:   "Einkommensteuer",
     CapTax:      "Ertragssteuer",
+    Tax:         "Steuern",
     NetIncome:   "Netto-Gewinn",
     PaidTax:     "Steuerforderung",
     FinYield:    "außerordentl.Ergebnis",
